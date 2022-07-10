e300a8de8c42f0bfd4f318dfc94091ca0d37cf29
clementtalleu <clement.talleu@gmail.com>
2022-07-10 11:49:13 +0200

    fix desc ytdlp

--- src/YoutubeDl.php
+++ src/YoutubeDl.php
@@ -202,7 +202,7 @@ class YoutubeDl
 
     protected function processDownload(Process $process): Video
     {
-        if (!preg_match('/Writing video description metadata as JSON to:\s(.+)/', $process->getOutput(), $m)) {
+        if (!preg_match('/Writing video (description)? metadata as JSON to:\s(.+)/', $process->getOutput(), $m)) {
             throw new YoutubeDlException('Failed to detect metadata file.');
         }
 
